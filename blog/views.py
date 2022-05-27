adb2a8214b268c3f298d1de0371da03e1ace020b
Carrie8348 <maruko_carrie@hotmail.com>
2022-05-27 08:45:46 +0000

    fix css settings

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,6 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
-    
 
 class PostDetail(View):
 
@@ -20,7 +19,6 @@ class PostDetail(View):
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-
 
         return render(
             request,
@@ -34,8 +32,9 @@ class PostDetail(View):
             },
         )
     
+   
+    
     def post(self, request, slug, *args, **kwargs):
-
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
@@ -59,9 +58,9 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "commented": True,
-                "comment_form": comment_form,
+                "commented": False,
                 "liked": liked,
+                "comment_form": CommentForm(),
             },
         )
     
@@ -76,5 +75,7 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
 
 
